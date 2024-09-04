614ccbd98d2137238839b91daadd1d28085d7bc6
2516e <mahleb.enzo@gmail.com>
2024-09-04 17:15:21 +0200

    Submit first

---
+++ public/header.html.php
@@ -0,0 +1,21 @@
+<header>
+
+    <?php include 'nav.html.php' ?>
+
+    <div class="hero">
+
+        <div class="left">
+            <h1><span class="gap">Welcome to</span><br>MO<span class="meow">MEOOW</span></h1>
+    
+            <p>Welcome to Momeoow, the ultimate destination for cat lovers! From cozy beds to playful toys, and gourmet treats, we have everything your feline friend needs to live their best nine lives. Treat your cat today – they’ll purr with joy! </p>
+            
+            <div class="btn">
+                <a href="">SHOP NEOW <i class="fa-solid fa-paw"></i></a>
+            </div>
+        </div>
+
+        <div class="heroimg">
+
+        </div>
+    </div>
+</header>
---
+++ public/nav.html.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="logo">
+        <img src="public/assets/img/image.png" alt="">
+    </div>
+
+    <ul>
+        <li><a href="">Shop Neow</a></li>
+        <li><a href="">Contact Us</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">cart</a></li>
+        <li><a href="">profil</a></li>
+    </ul>
+</nav>
