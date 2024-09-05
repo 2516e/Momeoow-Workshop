203837f9464874fb294f8a5cc81f162530561f5e
KevKash <kashamakevin270@gmail.com>
2024-09-05 17:01:07 +0200

    05/09/2024

---
+++ contact_us.php
@@ -0,0 +1,16 @@
+<?php include './public/header.html.php' ?>
+
+<main>
+    
+<h2>CONTACT US</h2>
+<section class="contact">
+
+
+
+<label for="text">Entrez votre message</label>
+<input type="text" id="inpu">
+
+
+</section> <input type="submit" id="btn">
+
+</main>
